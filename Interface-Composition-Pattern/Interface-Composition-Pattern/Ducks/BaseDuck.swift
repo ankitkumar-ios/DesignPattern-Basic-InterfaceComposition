e10962459fd886b72476a4e9c20c864e61daedad
Ankit Kumar <ankit.kumar31121993@gmail.com>
2022-07-28 10:21:24 +0530

    Adding new function or removing will have to update all ducks subclasses

--- Interface-Composition-Pattern/Interface-Composition-Pattern/Ducks/BaseDuck.swift
+++ Interface-Composition-Pattern/Interface-Composition-Pattern/Ducks/BaseDuck.swift
@@ -23,17 +23,61 @@ class BaseDuck: NSObject {
 }
 
 class MallardDuck: BaseDuck {
-
+    override func quack() {
+        print("Mallard quack")
+    }
+    override func display() {
+        print("Mallard display")
+    }
+    override func swim() {
+        print("Mallard swim")
+    }
+    override func fly() {
+        print("Mallard fly")
+    }
 }
 
 class RedheadDuck: BaseDuck {
-
+    override func quack() {
+        print("Redhead quack")
+    }
+    override func display() {
+        print("Redhead display")
+    }
+    override func swim() {
+        print("Redhead swim")
+    }
+    override func fly() {
+        print("Redhead fly")
+    }
 }
 
 class RubberDuck: BaseDuck {
-
+    override func quack() {
+        print("Rubber quack")
+    }
+    override func display() {
+        print("Rubber display")
+    }
+    override func swim() {
+        print("Rubber swim")
+    }
+    override func fly() {
+        print("Rubber fly")
+    }
 }
 
 class DecoyDuck: BaseDuck {
-
+    override func quack() {
+        print("Decoy quack")
+    }
+    override func display() {
+        print("Decoy display")
+    }
+    override func swim() {
+        print("Decoy swim")
+    }
+    override func fly() {
+        print("Decoy fly")
+    }
 }
